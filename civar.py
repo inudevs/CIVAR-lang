f1084fd52dd48cf8009889ce92bcee4818fcb528
suhdonghwi <hwidongsuh@gmail.com>
2019-01-07 02:23:15 +0900

    정규표현식 활용 해석으로 변경, 예시 추가

--- civar.py
+++ civar.py
@@ -1,89 +1,95 @@
+import argparse
+import re
 from enum import Enum
 
 class State(Enum):
     none = 0
-    civar_c = 1
-    civar_i = 2
-    civar_var = 3
+    civar = 1
+    dot = 2
+    comma = 3
     ha = 4
-    ah = 5
-    dot = 6
-    comma = 7
+    space = 5
 
 def interpret_civar(line):
-    current_num = 0
+    parser_exp = re.compile(r'[.]+|[,]+|시이*바|하아*|아+| +')
+    parse_result = parser_exp.findall(line)
+
     civar_count = 0
     dot_count = 0
     comma_count = 0
 
+    def tag_block(block):
+        if block.startswith('시'):
+            return State.civar
+        elif block.startswith('.'):
+            return State.dot
+        elif block.startswith(','):
+            return State.comma
+        elif block.startswith(('하', '아')):
+            return State.ha
+        elif block.startswith(' '):
+            return State.space
+
     current_state = State.none
-    char_state_map = {' ' : State.none,
-                      '시' : State.civar_c,
-                      '이' : State.civar_i,
-                      '바' : State.civar_var,
-                      '하' : State.ha,
-                      '아' : State.ah,
-                      '.' : State.dot,
-                      ',' : State.comma}
+    previous_state = State.none
 
+    current_num = 0
     final_str = ""
-    current_col = 0
-    current_row = 0
 
-    for char in line:
-        if (char == '시' and current_state == State.none) or \
-           (char == '이' and current_state in [State.civar_c, State.civar_i]) or \
-           (char == '바' and current_state in [State.civar_c, State.civar_i]):
-            civar_count += 1
-        elif char == '.':
-            if current_state in [State.civar_var, State.dot]:
-                dot_count += 1
-            elif current_state in [State.ha, State.ah]:
-                current_num += 1
-                continue
+    for block in parse_result:
+        current_state = tag_block(block)
+        block_len = len(block)
+        
+        if current_state == State.civar:
+            civar_count += block_len
 
-        elif char == ',':
-            if current_state in [State.civar_var, State.comma]:
-                comma_count += 1
-            elif current_state in [State.ha, State.ah]:
-                current_num -= 1
-                continue
+        elif current_state == State.dot:
+            if previous_state == State.civar:
+                dot_count += block_len
+            elif previous_state == State.ha:
+                current_num += block_len
 
-        elif char == ' ' and current_state in [State.dot, State.comma]:
+        elif current_state == State.comma:
+            if previous_state == State.civar:
+                comma_count += block_len
+            elif previous_state == State.ha:
+                current_num -= block_len
+
+        elif current_state == State.ha:
+            final_str += chr(current_num) * block_len
+
+        elif current_state == State.space:
             if dot_count > 0:
                 current_num += civar_count * dot_count
-            else:
+            elif comma_count > 0:
                 current_num -= civar_count * comma_count
 
             civar_count = 0
             dot_count = 0
             comma_count = 0
-            current_state = State.none
 
-        elif (char == '하' and current_state == State.none) or \
-             (char == '아' and current_state in [State.none, State.ha, State.ah]):
-            final_str += chr(current_num)
-
-            civar_count = 0
-            dot_count = 0
-            comma_count = 0
-
-        elif char == '\n':
-            current_col += 1
-            continue
-
-        else:
-            print("오류! : " + str(current_col + 1) + "행 " + str(current_row + 1) + "열에서 유효하지 않은 문자 발견")
-            exit()
-
-        current_state = char_state_map[char]
-        current_row += 1
-
+        previous_state = current_state
+    
     return final_str
 
-while True:
-    line = input('>> ').strip()
-    if line == ':quit':
-        break
-    
-    print(interpret_civar(line))
+def run_repl():
+    while True:
+        line = input('>> ').strip()
+        if line == ':quit':
+            break
+        
+        print(interpret_civar(line))
+
+def main():
+    parser = argparse.ArgumentParser(description="시바랭 실행기")
+    parser.add_argument("input", nargs='?', help="입력 파일의 경로")
+    args, _ = parser.parse_known_args()
+
+    if args.input is not None:
+        with open(args.input, 'r') as code_file:
+            print(interpret_civar(code_file.read()))
+    else:
+        run_repl()
+
+if __name__ == "__main__":
+    main()
